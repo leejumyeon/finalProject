f9678ca402af1737289fca2a308b6196d1a4f2bb
jonghyeoneg <68728734+jonghyeoneg@users.noreply.github.com>
2020-08-11 20:52:24 +0900

    commit - 최성수

--- GroupWare/src/main/java/com/spring/groupware/seongsu/model/SeongsuDAO.java
+++ GroupWare/src/main/java/com/spring/groupware/seongsu/model/SeongsuDAO.java
@@ -1,5 +1,8 @@
 package com.spring.groupware.seongsu.model;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class SeongsuDAO {
 	
 }
--- GroupWare/src/main/java/com/spring/groupware/seongsu/service/SeongsuService.java
+++ GroupWare/src/main/java/com/spring/groupware/seongsu/service/SeongsuService.java
@@ -2,6 +2,9 @@ package com.spring.groupware.seongsu.service;
 
 import java.util.HashMap;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SeongsuService implements InterSeongsuService {
 
 	@Override
